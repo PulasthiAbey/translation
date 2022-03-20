2ab36c8a3585513c979f02e82f04e5ef34243b24
PulasthiAberathne <pulasthi.aberathne@1billiontech.com>
2022-03-20 09:26:23 +0530

    project initiated

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div>Header goes here</div>
+    </div>
+  );
+};
+
+export default Header;
