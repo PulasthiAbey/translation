2114e0c36141c05efbd22980eb45970356077000
PulasthiAberathne <pulasthi.aberathne@1billiontech.com>
2022-03-20 16:05:20 +0530

    input componenets added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
 import "./App.css";
+import InputFields from "./components/InputFields/InputFields";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <InputFields />
     </div>
   );
 }
