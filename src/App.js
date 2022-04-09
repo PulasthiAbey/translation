55592d53173858c53d2a8d7db358f5d36af12f68
PulasthiAberathne <pulasthi.aberathne@1billiontech.com>
2022-04-09 21:28:54 +0530

    screen changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import Header from "./components/Header/Header";
 import "./App.css";
-import InputFields from "./components/InputFields/InputFields";
-import BottomIcons from "./components/BottomIcons/BottomIcons";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <InputFields />
-      <BottomIcons />
+      
     </div>
   );
 }
---
+++ src/screens/ConverterScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import InputFields from "../components/InputFields/InputFields";
+import BottomIcons from "../components/BottomIcons/BottomIcons";
+
+const ConverterScreen = () => {
+  return (
+    <>
+      <Header />
+      <InputFields />
+      <BottomIcons />
+    </>
+  );
+};
+
+export default ConverterScreen;
