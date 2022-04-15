65299ec0e18c3741ff1e84d4ad3c7b81626179d1
PulasthiAberathne <pulasthi.aberathne@1billiontech.com>
2022-04-16 01:35:56 +0530

    backend

---
+++ CRITR/main_vision.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+
+app.listen(5000, '127.0.0.1', ()=> console.log('Server running'));
--- src/components/HeaderHome/Header.js
+++ src/components/HeaderHome/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 
 import "./header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -9,7 +10,9 @@ const Header = () => {
       <div className="header-left">CRITR.</div>
       <div className="header-right">
         <div className="convert-text">Home</div>
-        <div className="convert-text">Converter</div>
+        <Link to={`/convert`}>
+          <div className="convert-text">Converter</div>
+        </Link>
         <div className="convert-text">About</div>
         <div className="menu-icon">
           <MenuIcon />
