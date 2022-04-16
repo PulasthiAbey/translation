f448bf6e297d538f8df115dcb7a4ff49ea97230f
SteffZ3 <assignmenthelper.se@gmail.com>
2022-04-16 19:25:55 +0530

    setup the translation rout e

---
+++ backend/CREDENTIALS.js
@@ -0,0 +1,14 @@
+module.exports.CREDENTIALS = {
+  type: "service_account",
+  project_id: "pkt-sniffer",
+  private_key_id: "888f41e6ee27222c3124203b55975d300e8d7dfa",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCc5yLU/LCbiz/w\nFjUYjmn1QRNeUQsSXluNwoiMhkhhxUM1rMsQ04JrJfHcp4qdpZT4WGcj393NkWpC\nrdkYmJJ4PyGX4FNR393tD4wTAqspyv+zRO5JjTu5bmQQEKHuBNwR6KeVBniKSpTq\n1iLuUmgK+6Gf3C4c/Awu/4AyGQoEfr4BKhuadKzM3m6nUSc5PSZl6VGrmDd1jbHY\n6MJ7U/FICwF0mDOOsYrCAxrCKJvHFn5f4hGtYLJwWFyvgPf84HpbjghydSFKa+OK\nsh+tacAMZ+v716eUioltJ4c077FH6JaQrKbln50ulKuF8djhxfz5e1DBqIFk0su3\nTappJYNBAgMBAAECggEACBjvDHkIAkS3ihY34kXn1rPC/cW9qsA6mFvN/K1y3UYA\nRZnMYDP/7SHVr4TokoYULbkwhXmD+9MBlM/L4u2R9rKQhVVkMI3f6RcnYvnGWk9O\nc2BfG4p/eRELm2PIWwaDCUnbRw7TOCdIfS3U8YZtESpSnfOh8nZGrUMbpF7PcOVw\nBxyi2iZNuc2OdaDenjXQ1NdnfjNDA7Y3k++Vfv/ms1ojSB7rErQfX1q9RTSr/S4T\nkSomCvZSEF0eSzZ8GsFc8R0m955kvjv8P0s43Z4zKsZuWEcw1BKSK5/3fgNASU4U\nPQZ4YOx9q58KhY20FxQa/Z2SG/FFzrtC0IC5v4/RywKBgQDR1NiEwaXPjimSF62E\nGMul3A0INFvwANHfupVAo1V9JwWX5mJbDGFtmt9egFGzXtwBkDjuyiSyLTa0+6sr\nKvpPC3VSJPg/MnQpOCSg44FpAET45kZ2BTIb9XJbbNvO+mMe+tNGLJJoQU5dEBSB\n5fjRbymZr7DXYcOQl4sMEXEsMwKBgQC/bP0mHgwZ/SzlnnzF+dixCHuh6vvv5/36\nn2ikBFF50w0NY4GJj3wDhdHDsJV1g/P++XPKoUqpSo0wG9bYtk09HNNbj+6/aZkt\nFdc+P3y0EofCdu8v6weooxK4cTib39cmTtYIeezWMpn+HnrHy2THBf36wJflstrz\n4pmBVkLeuwKBgDgMpPGBUKtKjCwsCvt4LB3X09tXMR/A+E7M5kVn29QcclALCEbN\n3dCLSBNUL368XgRE5BBVCsxVa4GYoDZdupMWunYAzhcDgSfpiU84tj64BbR+vl+U\nL7hsLeniAC0fkQ6bbYgXK9yRZT9Spke0iGxZR0F4PsKpqLWc5VWJ6zMlAoGAAykP\nfOOeG4e8iAX31WTsmO+k6uVwR2OzI8Ih/iM8HSDI5GGyL804pO4+qlZuAUSucJU5\nviyviN/8OMEc6PMJj2q8apSTwoFB0Ca+KP7vViHAMAu6ozXkKHqYwobMAnbNnIpM\nqlMo/9hSBooGSOoovchxiwRG/bnYN5KUKjh8XiUCgYAcs/kWznvNFFcSijsiyMwB\n3wbdLCRfth5xIpr3J13mdsNn5KGuVastvSYhTWUAYjuXTTOyDaKHmFB/rbgZlgOi\n8k5zsC0OBas7zPH1oMVCKnSpe1/oI+Cntpxtjqt8+PLUeu2dWnHC2Q1phY0gEt8X\nwNdvHi7Q5UQdY+AQDoQVFw==\n-----END PRIVATE KEY-----\n",
+  client_email: "testuser@pkt-sniffer.iam.gserviceaccount.com",
+  client_id: "109772913648034245922",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/testuser%40pkt-sniffer.iam.gserviceaccount.com",
+};
--- backend/routes/translate.js
+++ backend/routes/translate.js
@@ -1,11 +1,11 @@
 const { Translate } = require("@google-cloud/translate").v2;
-require("dotenv");
+require("dotenv").config;
 
-const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+const { CREDENTIALS } = require("../CREDENTIALS");
 
 const translate = new Translate({
   credentials: CREDENTIALS,
-  projectId: CREDENTIALS.project_Id,
+  projectId: CREDENTIALS.project_id,
 });
 
 const detectLanguage = async (text) => {
@@ -13,12 +13,25 @@ const detectLanguage = async (text) => {
     let response = await translate.detect(text);
     return response[0].language;
   } catch (error) {
-    console.log(`Error at detecting the language ${error}`);
+    console.log(`Error at detecting the language ==> ${error}`);
     return 0;
   }
 };
 
 const translateText = async (text, targetLanguage) => {
   try {
-  } catch (error) {}
+    let [response] = await translate.translate(text, targetLanguage);
+    return response;
+  } catch (error) {
+    console.log(`Error at translating the language ==> ${error}`);
+    return 0;
+  }
 };
+
+translateText("Oggi lunedi", "en")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
