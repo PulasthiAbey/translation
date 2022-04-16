e139e8be1c988eec2d76a880adb5f7cd241ee89b
himaruwan2pulasthi@gmail.com <assignmenthelper.se@gmail.com>
2022-04-16 20:03:43 +0530

    UI changes

--- frontend/src/components/InputFields/InputFields.js
+++ frontend/src/components/InputFields/InputFields.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ChangeCircleIcon from "@mui/icons-material/ChangeCircle";
 
 import "./InputFields.css";
 
 const InputFields = () => {
+  const [translatedText, setTranslatedText] = useState("");
+  const [fetchResponse, setFetchResponse] = useState({});
+  const [text, setText] = useState("");
+
+  const translate = () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text: text }),
+    };
+
+    fetch("http://localhost:9090/translate", requestOptions)
+      .then((response) => {
+        setFetchResponse(response);
+      })
+      .catch((error) => {
+        alert(
+          `Error Occurred while trying to translate ${error.message}\nError ==> ${error}`
+        );
+      });
+
+    if (fetchResponse.status === "SUCCESS") {
+      setTranslatedText(fetchResponse.translatedText);
+    } else {
+      alert(
+        `Error Occurred while trying to translate\nError ==> ${fetchResponse}`
+      );
+    }
+  };
+
   return (
     <>
       <div className="padding-top" />
@@ -16,21 +46,19 @@ const InputFields = () => {
             id="leftInput"
             placeholder="Paste Text here"
             className="field"
+            onChange={(e) => setText(e.target.value)}
           />
           {/* </div> */}
         </div>
         <div className="icon-middle">
-          <button
-            className="button"
-            //   onClick={alert("Translate Started")}
-          >
+          <button className="button" onClick={translate()}>
             <ChangeCircleIcon fontSize="large" />
           </button>
         </div>
         <div className="right-field">
           <div className="field-header">English</div>
           {/* <div className="border"> */}
-          <div className="field" id="translated"></div>
+          <div className="field" id="translated" value={translatedText}></div>
           {/* </div> */}
         </div>
       </div>
