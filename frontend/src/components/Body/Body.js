d4bb010fc9cd4829fd1ef4016fd418c5ead1ecc5
PulasthiAbey <himaruwan2pualsthi@gmail.com>
2022-04-16 22:10:56 +0530

    ui changes

--- frontend/src/components/Body/Body.js
+++ frontend/src/components/Body/Body.js
@@ -4,7 +4,22 @@ import "./Body.css";
 const Body = () => {
   return (
     <>
-      <div className="container"></div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <img
+              className="img-responsive"
+              src={require("../../assets/images/homeImage.png")}
+              alt="bodyImage"
+            />
+          </div>
+          <div className="col">
+            <div className="body-text">
+              Character & Typeface Recognition in One Tool
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
